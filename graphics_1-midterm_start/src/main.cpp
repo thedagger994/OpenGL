f70c0561d8a19b73c4d15be857baf4f2747789f2
Daniel Bassili <122551444+thedagger994@users.noreply.github.com>
2024-10-16 16:05:18 -0400

    Added Midterm start

---
+++ graphics_1-midterm_start/src/main.cpp
@@ -0,0 +1,172 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "Math.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cassert>
+
+void OnResize(GLFWwindow* window, int width, int height);
+void OnInput(GLFWwindow* window);
+
+const unsigned int SCR_WIDTH = 1920;
+const unsigned int SCR_HEIGHT = 1080;
+
+GLuint CreateShader(GLint type, const char* path)
+{
+    GLuint shader = 0;
+    try
+    {
+        // Load text file
+        std::ifstream file;
+        file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        file.open(path);
+
+        // Interpret the file as a giant string
+        std::stringstream stream;
+        stream << file.rdbuf();
+        file.close();
+
+        // Verify shader type matches shader file extension
+        const char* ext = strrchr(path, '.');
+        switch (type)
+        {
+        case GL_VERTEX_SHADER:
+            assert(strcmp(ext, ".vert") == 0);
+            break;
+
+        case GL_FRAGMENT_SHADER:
+            assert(strcmp(ext, ".frag") == 0);
+            break;
+        default:
+            assert(false, "Invalid shader type");
+            break;
+        }
+
+        // Compile text as a shader
+        std::string str = stream.str();
+        const char* src = str.c_str();
+        shader = glCreateShader(type);
+        glShaderSource(shader, 1, &src, NULL);
+        glCompileShader(shader);
+
+        // Check for compilation errors
+        GLint success;
+        GLchar infoLog[512];
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(shader, 512, NULL, infoLog);
+            std::cout << "Shader failed to compile: \n" << infoLog << std::endl;
+        }
+    }
+    catch (std::ifstream::failure& e)
+    {
+        std::cout << "Shader (" << path << ") not found: " << e.what() << std::endl;
+    }
+    return shader;
+}
+
+GLuint CreateProgram(GLuint vs, GLuint fs)
+{
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vs);
+    glAttachShader(shaderProgram, fs);
+    glLinkProgram(shaderProgram);
+
+    // Check for linking errors
+    int success;
+    char infoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        shaderProgram = GL_NONE;
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+
+    return shaderProgram;
+}
+
+struct Vertex
+{
+    Vector3 position;
+    Vector3 color;
+};
+
+using Vertices = std::vector<Vertex>;
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, OnResize);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    GLuint vsDefault = CreateShader(GL_VERTEX_SHADER, "./assets/shaders/Default.vert");
+    GLuint fsDefault = CreateShader(GL_FRAGMENT_SHADER, "./assets/shaders/Default.frag");
+    GLuint shaderDefault = CreateProgram(vsDefault, fsDefault);
+    glUseProgram(shaderDefault);
+
+    Vector3 positions[] = {
+        { -1.0f, -1.0f, 0.0f },
+        { 1.0f, -1.0f, 0.0f },
+        { 0.0f, 1.0f, 0.0f }
+    };
+
+    Vector3 colors[] = {
+        { 1.0f, 0.0f, 0.0f },
+        { 0.0f, 1.0f, 0.0f },
+        { 0.0f, 0.0f, 1.0f }
+    };
+
+    // Task 1 -- loop through all vertices to assign positions and colors
+    Vertices vertices(30000);
+
+    // Task 2 -- upload vertices to the GPU
+    GLuint vao, vbo;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        OnInput(window);
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glDrawArrays(GL_POINTS, 0, vertices.size());
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void OnInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void OnResize(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
